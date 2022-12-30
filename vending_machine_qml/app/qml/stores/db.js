310c1e4cb20fb35014123705e95cce100ee61815
Ernesto Cruz <ecruzolivera@gmail.com>
2022-12-29 18:12:40 -0600

    using sane prices

--- vending_machine_qml/app/qml/stores/db.js
+++ vending_machine_qml/app/qml/stores/db.js
@@ -28,147 +28,147 @@ function db() {
                 "categoryId": "1",
                 "name": "burger",
                 "description": "Officia enim amet enim sit nostrud quis voluptate culpa et ut ullamco exercitation quis.",
-                "price":3684,
+                "price":368,
                 "image": "qrc:/burger.svg"
             }, {
                 "id": "2",
                 "categoryId": "1",
                 "name": "chocolatebar",
                 "description": "Fugiat nisi ea amet irure dolore in pariatur laborum commodo mollit cupidatat.",
-                "price":3401,
+                "price":340,
                 "image": "qrc:/chocolatebar.svg"
             }, {
                 "id": "3",
                 "categoryId": "1",
                 "name": "fried chicken",
                 "description": "Cupidatat do adipisicing elit culpa mollit laboris mollit adipisicing ipsum est nostrud excepteur ex mollit.",
-                "price":2913,
+                "price":291,
                 "image": "qrc:/fried-chicken.svg"
             }, {
                 "id": "4",
                 "categoryId": "1",
                 "name": "hot dog",
                 "description": "Aliquip laboris elit aliquip voluptate sunt labore quis Lorem deserunt.",
-                "price":1123,
+                "price":112,
                 "image": "qrc:/hot-dog.svg"
             }, {
                 "id": "5",
                 "categoryId": "1",
                 "name": "pizza",
                 "description": "Adipisicing do laborum labore eu do adipisicing minim sunt adipisicing est do.",
-                "price":5211,
+                "price":521,
                 "image": "qrc:/pizza.svg"
             }, {
                 "id": "6",
                 "categoryId": "1",
                 "name": "potato",
                 "description": "Elit veniam officia qui pariatur amet et aliquip sunt adipisicing duis ipsum pariatur dolore.",
-                "price":9822,
+                "price":982,
                 "image": "qrc:/potato.svg"
             }, {
                 "id": "7",
                 "categoryId": "1",
                 "name": "taco",
                 "description": "Pariatur duis anim veniam mollit ullamco Lorem.",
-                "price":4091,
+                "price":409,
                 "image": "qrc:/taco.svg"
             }, {
                 "id": "8",
                 "categoryId": "1",
                 "name": "hamburger",
                 "description": "Qui reprehenderit ullamco fugiat aliqua.",
-                "price":3235,
+                "price":323,
                 "image": "qrc:/hamburger.svg"
             }, {
                 "id": "9",
                 "categoryId": "1",
                 "name": "popcorn",
                 "description": "Pariatur aute irure occaecat consectetur mollit esse est ut consequat dolore cupidatat laborum tempor minim.",
-                "price":5859,
+                "price":585,
                 "image": "qrc:/popcorn.svg"
             }, {
                 "id": "10",
                 "categoryId": "1",
                 "name": "sandwich",
                 "description": "Duis nostrud irure laboris aute.",
-                "price":4461,
+                "price":446,
                 "image": "qrc:/sandwich.svg"
             }, {
                 "id": "11",
                 "categoryId": "1",
                 "name": "shortcake",
                 "description": "Exercitation exercitation Lorem est adipisicing aute esse ad et.",
-                "price":8355,
+                "price":835,
                 "image": "qrc:/shortcake.svg"
             }, {
                 "id": "12",
                 "categoryId": "2",
                 "name": "apricot",
                 "description": "Reprehenderit est in fugiat sit culpa aliqua.",
-                "price":4588,
+                "price":458,
                 "image": "qrc:/apricot.svg"
             }, {
                 "id": "13",
                 "categoryId": "2",
                 "name": "avocado",
                 "description": "Enim cupidatat cupidatat minim qui tempor id sit sunt.",
-                "price":8922,
+                "price":892,
                 "image": "qrc:/avocado.svg"
             }, {
                 "id": "14",
                 "categoryId": "2",
                 "name": "banana",
                 "description": "Commodo Lorem pariatur non eu duis duis velit.",
-                "price":1636,
+                "price":163,
                 "image": "qrc:/banana.svg"
             }, {
                 "id": "15",
                 "categoryId": "2",
                 "name": "dragon fruit",
                 "description": "Cillum nostrud ut adipisicing excepteur non est deserunt sit do veniam aliqua.",
-                "price":5900,
+                "price":590,
                 "image": "qrc:/dragon.svg"
             }, {
                 "id": "16",
                 "categoryId": "2",
                 "name": "durian",
                 "description": "Proident ad labore eu consequat proident officia mollit minim reprehenderit Lorem cillum dolore cupidatat quis.",
-                "price":3857,
+                "price":385,
                 "image": "qrc:/durian.svg"
             }, {
                 "id": "17",
                 "categoryId": "2",
                 "name": "granate",
                 "description": "Incididunt pariatur ipsum cillum pariatur cillum id nisi qui laboris elit laboris duis commodo est.",
-                "price":8856,
+                "price":885,
                 "image": "qrc:/granate.svg"
             }, {
                 "id": "18",
                 "categoryId": "2",
                 "name": "grape",
                 "description": "Sint adipisicing do est commodo ipsum.",
-                "price":9045,
+                "price":904,
                 "image": "qrc:/grape.svg"
             }, {
                 "id": "19",
                 "categoryId": "2",
                 "name": "kiwi",
                 "description": "Nisi non nulla proident velit.",
-                "price":4813,
+                "price":481,
                 "image": "qrc:/kiwi.svg"
             }, {
                 "id": "20",
                 "categoryId": "2",
                 "name": "lychee",
                 "description": "Est sint amet laboris mollit magna irure dolor nulla do fugiat in.",
-                "price":6430,
+                "price":643,
                 "image": "qrc:/lychee.svg"
             }, {
                 "id": "21",
                 "categoryId": "2",
                 "name": "mango",
                 "description": "Exercitation irure Lorem magna ullamco mollit veniam do excepteur esse aliquip ut.",
-                "price":7847,
+                "price":784,
                 "image": "qrc:/mango.svg"
             }, {
                 "id": "22",
@@ -182,28 +182,28 @@ function db() {
                 "categoryId": "2",
                 "name": "pineapple",
                 "description": "Eiusmod laborum qui Lorem eiusmod in magna cupidatat nulla non in.",
-                "price":7383,
+                "price":738,
                 "image": "qrc:/pineapple.svg"
             }, {
                 "id": "24",
                 "categoryId": "2",
                 "name": "raspberry",
                 "description": "Consequat dolore laboris pariatur cillum nulla ad aliquip magna irure consequat cupidatat cupidatat do.",
-                "price":2239,
+                "price":223,
                 "image": "qrc:/raspberry.svg"
             }, {
                 "id": "25",
                 "categoryId": "2",
                 "name": "strawberry",
                 "description": "Pariatur qui est pariatur excepteur duis deserunt ex ipsum irure aliquip nisi amet tempor.",
-                "price":6003,
+                "price":600,
                 "image": "qrc:/strawberry.svg"
             }, {
                 "id": "26",
                 "categoryId": "2",
                 "name": "watermelon",
                 "description": "Magna ut excepteur sit dolor.",
-                "price":7259,
+                "price":725,
                 "image": "qrc:/watermelon.svg"
             }, {
                 "id": "27",
@@ -217,84 +217,84 @@ function db() {
                 "categoryId": "3",
                 "name": "cherry",
                 "description": "Tempor culpa ad magna deserunt sunt laboris ullamco aute magna mollit fugiat reprehenderit culpa ipsum.",
-                "price":2549,
+                "price":254,
                 "image": "qrc:/cherry.svg"
             }, {
                 "id": "29",
                 "categoryId": "3",
                 "name": "coconut",
                 "description": "Ullamco qui occaecat velit culpa laborum eiusmod do amet in consectetur dolore.",
-                "price":7887,
+                "price":788,
                 "image": "qrc:/coconut.svg"
             }, {
                 "id": "30",
                 "categoryId": "3",
                 "name": "guava",
                 "description": "Eiusmod ad culpa voluptate commodo nisi aute ullamco ad dolore.",
-                "price":7339,
+                "price":733,
                 "image": "qrc:/guava.svg"
             }, {
                 "id": "31",
                 "categoryId": "3",
                 "name": "lemon",
                 "description": "Ipsum consectetur ut cupidatat et veniam anim reprehenderit quis pariatur dolor officia nulla ipsum Lorem.",
-                "price":5874,
+                "price":587,
                 "image": "qrc:/lemon.svg"
             }, {
                 "id": "32",
                 "categoryId": "3",
                 "name": "mango",
                 "description": "Cillum aliquip excepteur quis sint deserunt fugiat excepteur laborum magna do culpa nostrud.",
-                "price":9967,
+                "price":996,
                 "image": "qrc:/mango.svg"
             }, {
                 "id": "33",
                 "categoryId": "3",
                 "name": "peach",
                 "description": "Ad veniam sunt consequat consequat.",
-                "price":5314,
+                "price":531,
                 "image": "qrc:/peach.svg"
             }, {
                 "id": "34",
                 "categoryId": "3",
                 "name": "pear",
                 "description": "Excepteur consequat cupidatat sit dolore deserunt duis amet.",
-                "price":2518,
+                "price":251,
                 "image": "qrc:/pear.svg"
             }, {
                 "id": "35",
                 "categoryId": "3",
                 "name": "pineapple",
                 "description": "In ullamco ut irure non commodo exercitation mollit ipsum laboris proident nulla.",
-                "price":2370,
+                "price":237,
                 "image": "qrc:/pineapple.svg"
             }, {
                 "id": "36",
                 "categoryId": "3",
                 "name": "pitahaya",
                 "description": "Aute consectetur minim ea nostrud enim id.",
-                "price":8567,
+                "price":856,
                 "image": "qrc:/pitahaya.svg"
             }, {
                 "id": "37",
                 "categoryId": "3",
                 "name": "strawberry",
                 "description": "Commodo pariatur tempor ut consectetur laborum deserunt proident pariatur mollit dolor nulla eu.",
-                "price":5647,
+                "price":564,
                 "image": "qrc:/strawberry.svg"
             }, {
                 "id": "38",
                 "categoryId": "4",
                 "name": "bandage",
                 "description": "Elit amet incididunt ut magna pariatur in.",
-                "price":3639,
+                "price":363,
                 "image": "qrc:/bandage.svg"
             }, {
                 "id": "39",
                 "categoryId": "4",
                 "name": "comb",
                 "description": "Eu do enim laborum laboris pariatur id do anim.",
-                "price":3204,
+                "price":320,
                 "image": "qrc:/comb.svg"
             }, {
                 "id": "40",
@@ -308,14 +308,14 @@ function db() {
                 "categoryId": "4",
                 "name": "soap",
                 "description": "Enim est nostrud exercitation deserunt adipisicing quis mollit in fugiat excepteur do.",
-                "price":9649,
+                "price":964,
                 "image": "qrc:/soap.svg"
             }, {
                 "id": "42",
                 "categoryId": "4",
                 "name": "usb drive",
                 "description": "Aliquip velit eiusmod eu esse tempor sit culpa nulla do est.",
-                "price":6306,
+                "price":630,
                 "image": "qrc:/usb_drive.svg"
             }]
     }
