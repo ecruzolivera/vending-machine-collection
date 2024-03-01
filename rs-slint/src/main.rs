00bc7a60c9d51a303ae188cef4982e7e4f109d24
Ernesto Cruz <ecruzolivera@gmail.com>
2024-03-01 10:24:11 -0600

    slint version updated

--- rs-slint/src/main.rs
+++ rs-slint/src/main.rs
@@ -1,7 +1,26 @@
+use env_logger::Builder;
+use log;
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
+    Builder::new().filter_level(log::LevelFilter::max()).init();
+    log::info!("starting ui");
+
     let ui = AppWindow::new()?;
+
+    ui.global::<CategoryCardCallback>()
+        .on_categorySelected(|category_id| {
+            log::info!("selected category {}", category_id);
+        });
+
+    ui.global::<ItemCardCallback>().on_itemInc(|item_id| {
+        log::info!("item incremented {}", item_id);
+    });
+
+    ui.global::<ItemCardCallback>().on_itemDec(|item_id| {
+        log::info!("item decremented {}", item_id);
+    });
 
     ui.run()
 }
